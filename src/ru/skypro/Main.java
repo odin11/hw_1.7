cdf13fae2b7e6cbfee40e897868f1f763b5294e1
odin11 <odinufa2014@gmail.com>
2022-01-20 16:46:05 +0300

    first commit (в 4 задании, на мой взгляд достаточно одним replace обойтись, но условие...)

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,5 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    //task1
+        String firstName = "Johan";
+        String middleName = "Lvovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName ;
+        System.out.println("ФИО сотрудника — "+ fullName);
+
+    //task2
+        String forBuhOtdel = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + forBuhOtdel);
+
+    //task3
+        String forAdmOtdel = fullName.replace(" ",";");
+        System.out.println("Данные ФИО сотрудника для административного отдела — " + forAdmOtdel);
+
+    //task4
+        //"Может потребоваться метод split() и метод contains()."
+        String fullname4 = "Иванов Семён Семёнович";
+        if (fullname4.contains("ё")) {
+            String fullnameE = fullname4.replace("ё","е");
+            System.out.println("Данные ФИО сотрудника — " + fullnameE + ".");
+        }else{
+            System.out.println("Данные ФИО сотрудника — " + fullname4 + ".");
+        }
+
     }
 }
